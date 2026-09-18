bb45915f85aac0785aa984e31c928d0f16db79eb
Dominik-Alge <dominik.alge@bluewin.ch>
2026-09-18 13:17:59 +0200

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// Erzwingt, dass die Tailwind-Klassen verarbeitet werden
+const style = document.createElement('style');
+style.textContent = `@import url('https://jsdelivr.net');`;
+document.head.appendChild(style);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
